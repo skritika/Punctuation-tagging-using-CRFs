a796a8eb5e0838b84313598b571824422c5aee3d
skritika <kritikasingh@gmail.com>
2014-02-02 20:08:33 -0800

    Added POS tagging function

--- helper.py
+++ helper.py
@@ -1,10 +1,14 @@
+import nltk.tag
+
 def load_data(file_path):
 	f = open(file_path, 'r')
 	list_data = f.readlines()
 	return list_data	
 
-
-	
- 
-
-
+def POS(sentence):
+	tokens = nltk.word_tokenize(sentence)
+	pos = nltk.pos_tag(tokens)
+	tags = []
+	for each in pos:
+		tags.append(each[1])
+	return tags
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from helper import *
 
 def train(train_data, train_labels):
-	'''trains model using the training data'''	
-	print train_data[0]
-	print train_labels[0]
-
+	'''Trains model using the training data'''	
+	for i in range(len(train_data)):
+		l = train_labels[i].split(' ')
+		if(i==100):
+			print POS(train_data[i]) 
+			print l
 
 def test(test_data, test_labels):
-	'''test on testing data'''	
-	print test_data[0]
-	print test_labels[0]
+	'''Function to test the trained model on test data'''	
+	#print test_data[0]
+	#print test_labels[0]
 
 train_data = load_data('punctuationDataset/trainingSentences.txt')
 train_labels = load_data('punctuationDataset/trainingLabels.txt')
@@ -17,6 +19,6 @@ test_data = load_data('punctuationDataset/testSentences.txt')
 test_labels = load_data('punctuationDataset/testLabels.txt')
 
 train(train_data, train_labels)
-test(test_data, test_labels)
+#test(test_data, test_labels)
 
 
