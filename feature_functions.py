42b51ffb93cd84ac16ffc74b35704483279c59a4
skritika <kritikasingh90@gmail.com>
2014-02-04 00:11:05 -0800

    Update feature_functions.py

--- feature_functions.py
+++ feature_functions.py
@@ -1,5 +1,5 @@
 # convention_ucsd: i = 1 denotes the first element in X, to acces it use X[0] not X[1]
-	1. The current word is 'and, but, yet, or, nor, for, so' and the previous tag is COMMA. 
+1. The current word is 'and, but, yet, or, nor, for, so' and the previous tag is COMMA. 
 2. The current word is 'however, consequently, otherwise, moreover, nevertheless', the previous tag is COMMA and the current tag is COMMA.
 3. The previous tag is 'PERIOD/EXCLAMATION_POINT/QUESTION_MARK' and the current tag is STOP.
 4. The previous tag is START, the current word is 'well, now, yes, moreover, furthermore, specifically, however, likewise, therefore, consequently, meanwhile'(transitional phrase) and the current tag is COMMA. Basically transitional phrase at the beginning of a sentence.
